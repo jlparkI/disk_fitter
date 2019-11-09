34a26c1e6d7b2a00b51525da4d3938d583e6d155
JParkinson <jlparkinson1@gmail.com>
2019-11-09 15:05:10 -0800

    added comments to modeler

--- scripts/modeler.py
+++ scripts/modeler.py
@@ -3,6 +3,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
+#If the user checked the appropriate flag, they may be importing MIC vs disk data or MIC vs MIC, and the function below
+#will try to determine which.
 def determine_data_type(qtapp):
 	try:
 		x = np.asarray([float(z) for z in qtapp.current_dataset['disks'].values])
@@ -12,19 +14,27 @@ def determine_data_type(qtapp):
 		return 'disk', 'Could not process the data! Column 2 contains negative values. Please reload and try again.'
 	if np.min(x) < 1 or np.max(x) > 60:
 		return 'mic', '0'
+	#MICs are nearly ALWAYS assigned to one of the values below based on microbiological standard practice.
+	#So if all or nearly all of the values in column 2 fall into one of these, then we're almost certainly dealing
+	#with MIC data. Disk values, by contrast, will generally be integers in [5,50]. 
 	xbins = {0.016,0.03,0.06,0.12,0.125,0.25,0.5,1,2,4,8,16,32,64,128,256}
 	likely_mic_values = 0
 	for xval in x:
 		if xval in xbins:
 			likely_mic_values += 1
+	#90% is a little arbitrary but this seems to work quite well in our experience so far...
 	if likely_mic_values > 0.9 * x.shape[0]:
 		return 'mic', '0'
 	return 'disk', '0'
 
 def process_traindata(raw, miccutoffR, miccutoffS):
+	#ysuscep is 1 if strain is susceptible, 0 if not. yresist is 1 if strain is resistant,
+	#0 if not. Intermediate strains have ysuscep=0, yresist=0.
         x, ysuscep, yresist = [], [], []
         for i in range(raw.shape[0]):
                 yreal = float(raw['mics'].values[i])
+		#We have to be careful about use of the >= and <= here. Microbiologists always specify for their cutoffs
+		#whether they are using >= or > and it matters since MICs are discrete value data not continuous.
                 if yreal >= miccutoffS and yreal < miccutoffR:
                         ysuscep.append(0)
                         yresist.append(0)
@@ -34,6 +44,8 @@ def process_traindata(raw, miccutoffR, miccutoffS):
                 elif yreal >= miccutoffR:
                         ysuscep.append(0)
                         yresist.append(1)
+	#The x-values -- the disk values -- are relatively straightforward since we do not have to assign categories
+	#for these.
         x = np.asarray(raw['disks'].values)
         ysuscep = np.asarray(ysuscep)
         xresist = np.asarray(yresist)
@@ -55,15 +67,20 @@ def fit_data(qtapp, data_type='disk'):
 		#If we couldn't do that, they PROBABLY entered numeric characters. Give 'em an error.
 		return "The data could not be processed. Typically this error results when it contains non-numeric characters (e.g. <=). Try again."
 
-	#Check to make sure their dataset really does contain both flavors. If not, give 'em error message 6.
+	#Check to make sure their dataset really does contain both flavors. If not, give 'em an error.
 	if np.unique(ysuscep).shape[0] == 1 or np.unique(yresist).shape[0] == 1:
 		return ("You are trying to fit data that either does not contain any resistant strains or does not contain any susceptible strains "
 				"(i.e. there are only resistant + intermediate or resistant + susceptible in this dataset). You could use "
 				"manual cutoff selection for this dataset. Check the manual override button to proceed.")
 	try:
+		#FOr both logistic regression and decision tree, fit two models, 
+		#one for susceptible vs rest, one for resistant vs rest. We do not need to use
+		#regularization here; sklearn's implementation of LogisticRegression doesn't let you disable
+		#regularization completely, but setting C to a very large value is basically the same
+		#thing since it makes the L2 term negligible.
 		if qtapp.use_logistic_regression == True:
-			suscep_vs_all = LogisticRegression(C=100, max_iter=10000)
-			resist_vs_all = LogisticRegression(C=100, max_iter=10000)
+			suscep_vs_all = LogisticRegression(C=1000, max_iter=10000)
+			resist_vs_all = LogisticRegression(C=1000, max_iter=10000)
 			if data_type == 'mic':
 				x = np.log(x)
 			suscep_vs_all.fit(x.reshape(-1,1), ysuscep)
@@ -109,6 +126,8 @@ def update_error_tables(qtapp, data_type='disk'):
 	micvalue = list(qtapp.current_dataset['mics'].values)
 	if data_type != 'mic':
 		for i in range(0, qtapp.current_dataset.shape[0]):
+			#Assign predictions and actual values to categories and populate the confusion matrix
+			#accordingly.
 			if micvalue[i] < miccutoffS:
 				actual_category = 0
 			elif micvalue[i] < miccutoffR:
@@ -123,6 +142,8 @@ def update_error_tables(qtapp, data_type='disk'):
 			else:
 				predicted_category = 2
 			qtapp.confusion_matrix[actual_category, predicted_category] += 1
+			#Here we have to be careful. FDA has specific definitions for 'very major error' and 'major error'.
+			#That's what we're using here.
 			if predicted_category != actual_category:
 				if actual_category == 1 or predicted_category == 1:
 					qtapp.error_counts['minor errors'] += 1
@@ -132,6 +153,8 @@ def update_error_tables(qtapp, data_type='disk'):
 					qtapp.error_counts['major errors'] += 1
 
 	elif data_type == 'mic':
+		#If the user imported MIC vs MIC data, the procedure is the same but the direction of the inequality
+		#for x values (what we're calling 'diskvalue' here for the sake of consistency) is reversed.
 		for i in range(0, qtapp.current_dataset.shape[0]):
 			if micvalue[i] < miccutoffS:
 				actual_category = 0
